9206d43068c93cddbcd58e841b5752718b9cb9a8
reyesjl <reyesjl.nine@hotmail.com>
2023-06-13 02:47:33 -0400

    view overhaul

--- f5/views.py
+++ f5/views.py
@@ -10,26 +10,56 @@ def index(request):
     '''
     Routes user to the landing view
     '''
-    return render(request, "index.html")
+    context = {
+        "location": "f5 landing page",
+        "motd": True,
+        "motd_message": "Welcome to F5Rugby.com! Explore our vintage jersey catalog, register for rugby camps, join rugby tours to Ireland, and stay informed with engaging articles. Proudly sponsored by Strong Lads, Canterbury of NZ, and World Rugby Shop."
+    }
+    return render(request, "index.html", context)
 
 def catalog(request):
-    return render(request, "catalog.html")
+    '''
+    Renders the catalog view
+    '''
+    context = {
+        "location": "f5/catalog"
+    }
+    return render(request, "catalog.html", context)
 
 def news(request):
-    latest_posts = Article.objects.order_by("title")
-    return render(request, "news.html")
+    '''
+    Renders the news view
+    '''
+    context = {
+        "location": "f5/news"
+    }
+    return render(request, "news.html", context)
 
 def camps(request):
-
-    # fetch camps...
-    latest_camps = RugbyCamp.objects.order_by("-startdate")
-    return render(request, "camps.html")
+    '''
+    Renders the camps view
+    '''
+    context = {
+        "location": "camps",
+        "motd": True,
+        "motd_message": "[Featured Camps] : Baltimore All Star Rugby Camp at Loyola Blakefield July 24 - 27, approx 4p - 10p daily."
+    }
+    return render(request, "camps.html", context)
 
 def tours(request):
-    
-    # fetch tours...
-
-    return render(request, "tours.html")
+    '''
+    Renders the tours view
+    '''
+    context = {
+        "location": "f5/tours"
+    }
+    return render(request, "tours.html", context)
 
 def info(request):
-    return render(request, "info.html")
+    '''
+    Renders the info view
+    '''
+    context = {
+        "location": "f5/info"
+    }
+    return render(request, "info.html", context)
