14244c5670ad02c9752f4f3db1cbfbfc941c1c0c
reyesjl <reyesjl.nine@hotmail.com>
2023-06-12 22:45:38 -0400

    registered my models properly

--- f5/admin.py
+++ f5/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import (
+    CustomUser, RugbyCamp, CampImage, 
+    Tour, TourImage, Article, ArticleImage, 
+    Tag, ApparelProduct, ProductImage
+) 
+
+
+admin.site.register(CustomUser)
+admin.site.register(RugbyCamp)
+admin.site.register(CampImage)
+admin.site.register(Tour)
+admin.site.register(TourImage)
+admin.site.register(Article)
+admin.site.register(ArticleImage)
+admin.site.register(Tag)
+admin.site.register(ApparelProduct)
+admin.site.register(ProductImage)
