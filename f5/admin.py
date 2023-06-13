d0d8133f002b7ef1b4d4ba54802d91b934160b07
reyesjl <reyesjl.nine@hotmail.com>
2023-06-13 00:14:25 -0400

    import all models

--- f5/admin.py
+++ f5/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import (
+from f5.models import (
     CustomUser, RugbyCamp, CampImage, 
     Tour, TourImage, Article, ArticleImage, 
     Tag, ApparelProduct, ProductImage
