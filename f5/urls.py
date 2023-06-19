ac96dceafd78a10d0c4110f9bc4b7a56aa09f234
reyesjl <reyesjl.nine@hotmail.com>
2023-06-19 04:27:48 -0400

    added articles url

--- f5/urls.py
+++ f5/urls.py
@@ -4,9 +4,20 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    
+    # Catalog match
     path("catalog/", views.catalog, name="catalog"),
+    
+    # News match
+    path('article/<int:id>/', views.article, name="article"),
     path("news/", views.news, name="news"),
+
+    # Camps match
     path("camps/", views.camps, name="camps"),
+
+    # Tours match
     path("tours/", views.tours, name="tours"),
+
+    # Info match
     path("info/", views.info, name="info"),
 ]
