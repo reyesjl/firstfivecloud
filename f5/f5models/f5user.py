d01a86a52421110e4993ee279213b988ad4e9be5
reyesjl <reyesjl.nine@hotmail.com>
2023-06-20 21:59:16 -0400

    user config extending AbstractUser

--- f5/f5models/f5user.py
+++ f5/f5models/f5user.py
@@ -1,3 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class F5user(AbstractUser):
+    # Additional rugby player attributes
+    email = models.EmailField(unique=True)
+    is_player = models.BooleanField(default=False)
+    cipp = models.CharField(max_length=8, unique=True, blank=True, null=True) 
