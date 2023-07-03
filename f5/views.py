e91153265a82c353315bcf9fe1d4bd2d52b89653
reyesjl <reyesjl.nine@hotmail.com>
2023-07-03 20:01:12 +0000

    about page; motd changes;

--- f5/views.py
+++ f5/views.py
@@ -11,7 +11,7 @@ def handleHomeRoute(request):
     context = {
         "location": "home",
         "motd": True,
-        "motd_message": "Welcome to www.firstfiverugby.com! Have a look around for deals, and stay a while :)",
+        "motd_message": "Welcome to www.firstfiverugby.com! Explore the platfrom and leave some feedback!",
         "pageTitle": "First Five Rugby",
         "pageSubtitle": "A premier portal dedicated to growing and developing the game of rugby in North America.",
     }
@@ -26,7 +26,7 @@ def handleAboutRoute(request):
         "motd": True,
         "motd_message": "Did you know that rugby is one of the fastest-growing sports in North America? With a staggering 40% increase in participation over the past five years, the passion for this incredible game is soaring.",
         "pageTitle":"About Us",
-        "pageSubtitle":"Revolutionizing rugby in North America through innovation, collaboration, and transformative experiences for players, driving game growth."
+        "pageSubtitle":"Revolutionizing rugby in North America, collaborating with local groups, and driving game growth."
     }
     return render(request, "about.html", context)
 
@@ -58,8 +58,8 @@ def handleCampsRoute(request):
         "location": "camps",
         "motd": True,
         "motd_message": "only 7 spots remaining for rugby camp [updated 23mins ago]",
-        "pageTitle": "Elevate Your Rugby",
-        "pageSubtitle": "Unlock Your Rugby Potential by Training Like a Pro Rugby Player: Empowering Youth, Building Community, and Nurturing Champions.",
+        "pageTitle": "Skills. Skills. Skills.",
+        "pageSubtitle": "Multi-day rugby camp with free kit, top level coaching, full days of rugby and prizes.",
         "camp":camp,
     }
     return render(request, "camps/index.html", context)
@@ -77,8 +77,8 @@ def handleToursRoute(request):
         "location": "tours",
         "motd": True,
         "motd_message": "our tours typically run 1/2 the price of others with amazing games, training, lodging, and culture",
-        "pageTitle": "The Ultimate Rugby Experience",
-        "pageSubtitle": "Explore Ireland, Play Against Local Teams, and Immerse Yourself in Rugby Culture: Unforgettable Team Tours to Ireland.",
+        "pageTitle": "Touring Ireland 2023",
+        "pageSubtitle": "Explore Ireland, Play Against Local Teams, and Immerse Yourself in Rugby Culture.",
         "tour": tour,
     }
     return render(request, "tours/index.html", context)
