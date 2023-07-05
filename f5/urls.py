1da2f2dc86d8024d09e1089e09eadf1c232415a3
reyesjl <reyesjl.nine@hotmail.com>
2023-07-05 15:55:16 +0000

    removed routes except for homepage; temporary

--- f5/urls.py
+++ f5/urls.py
@@ -4,18 +4,18 @@ from . import views
 
 urlpatterns = [
     # Root url 
-    path("", views.handleHomeRoute, name="index"),
+    path("", views.handleHomeRoute, name="home"),
 
     # About match
-    path("about/", views.handleAboutRoute, name="about"),
+    #path("about/", views.handleAboutRoute, name="about"),
 
     # Catalog match
-    path("catalog/", views.handleCatalogRoute, name="catalog"),
+    #path("catalog/", views.handleCatalogRoute, name="catalog"),
 
     # Camps match
-    path("camps/", views.handleCampsRoute, name="camps"),
+    #path("camps/", views.handleCampsRoute, name="camps"),
 
     # Tours match
-    path("tours/", views.handleToursRoute, name="tours"),
+    #path("tours/", views.handleToursRoute, name="tours"),
 ]
     
