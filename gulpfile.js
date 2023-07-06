7cc1fcf1fde7fd62cb023f10edd7b033ba2c5045
reyesjl <reyesjl.nine@hotmail.com>
2023-07-06 00:12:05 +0000

    badass gulp file; reloadserver alias not working ?

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
+// Description: Gulp tasks for compiling sass and running django server
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const shell = require('gulp-shell');
 
-// 
+// watches for changes in /static/sass
+gulp.task('watch', function() {
+  gulp.watch('f5/static/sass/**/*.scss', gulp.series('sass', 'collectstatic', 'reloadserver'));
+});
+
+// compiles sass from /static/sass to /static/css
 gulp.task('sass', function() {
   return gulp
     .src('f5/static/sass/**/*.scss')
@@ -10,10 +16,12 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('f5/static/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('f5/static/sass/**/*.scss', gulp.series('sass'));
-});
-
-gulp.task('collectstatic', shell.task('python manage.py collectstatic'));
-gulp.task('reloadserver', shell.task('reloadserver'));
+// compiles sass and watches for changes
 gulp.task('default', gulp.series('sass', 'watch'));
+
+// runs django server using shell alias
+gulp.task('reloadserver', shell.task('reloadserver'));
+
+// runs collectstatic to update static files; 
+// note: always overwrites due to --noinput
+gulp.task('collectstatic', shell.task('python manage.py collectstatic --noinput'));
