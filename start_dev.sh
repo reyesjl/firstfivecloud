ebf36016c0cbdeafede0cf1bf2f344db4e733998
reyesjl <reyesjl.nine@hotmail.com>
2023-07-12 01:56:14 +0000

    dev script for running my app

---
+++ start_dev.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Default values
+SHOWLOGS=false
+
+# Parse command line options
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --logs)
+    SHOWLOGS=true
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+echo "Stopping previous Sass watcher..."
+pkill -9 -f "sass --watch /home/django/firstfiverugby/f5/static/scss:/home/django/firstfiverugby/f5/static/css"
+
+echo "Starting new Sass watcher..."
+sass --watch /home/django/firstfiverugby/f5/static/scss:/home/django/firstfiverugby/f5/static/css &
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Restarting gunicorn and nginx..."
+sudo systemctl restart gunicorn.service
+sudo systemctl restart nginx.service
+
+if $SHOWLOGS ; then
+    echo "Showing logs for gunicorn and nginx..."
+    journalctl -u gunicorn.service -n 50 --no-pager
+    journalctl -u nginx.service -n 50 --no-pager
+else
+    echo "Status of gunicorn and nginx..."
+    sudo systemctl status gunicorn.service
+    sudo systemctl status nginx.service
+fi
