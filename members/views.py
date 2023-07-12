574c5db1ab57236671738e55c70bd4a57be1bed5
reyesjl <reyesjl.nine@hotmail.com>
2023-07-12 17:15:00 +0000

    handleloginView method psuedo

--- members/views.py
+++ members/views.py
@@ -6,6 +6,19 @@ def handleLoginUser(request):
     '''
     This view handles the login form.
     '''
+    if request.method == 'POST':
+        # Get the post parameters
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername, password=loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Successfully Logged In')
+            return redirect('home', {'user': user}})
+        else:
+            messages.error(request, 'Invalid Credentials, Please try again')
+            return redirect('login')
     return render(request, 'authenticate/login.html', {})
 
 def handleSignupUser(request):
