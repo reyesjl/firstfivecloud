40de4ffc20900f524283dccef3023ce503cbcdf7
reyesjl <reyesjl.nine@hotmail.com>
2023-07-12 13:53:04 +0000

    added memebrs and django authenticate to URLS

--- firstfiverugby/urls.py
+++ firstfiverugby/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', include("f5.urls")),
     path('f5/', include("f5.urls")),
     path('admin/', admin.site.urls),
+    path('members/', include("django.contrib.auth.urls")),
+    path('members/', include("members.urls")),
 ]
