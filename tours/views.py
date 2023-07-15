06aa0e9c5e29bccebbb66115510df9ae860bb53a
reyesjl <reyesjl.nine@hotmail.com>
2023-07-15 20:25:24 +0000

    all tours app related files

---
+++ tours/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def handleToursLanding(request):
+  """
+    Displays tours landing page
+  """
+  context = {"activelink": "tours"}
+  return render(request, "tours_welcome.html", context)
