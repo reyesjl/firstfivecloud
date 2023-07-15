36ec432210c2ad5f22bb5d7d7fcc79e2843fdcab
reyesjl <reyesjl.nine@hotmail.com>
2023-07-15 19:45:47 +0000

    added urls and views file

--- camps/views.py
+++ camps/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def handleWelcome(request):
+  """
+    Displays camps landing page
+  """
+  context = {"activelink": "camps"}
+  return render(request, "events/welcome.html", context)
