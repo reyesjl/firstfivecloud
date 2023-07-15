502a1aece15d0701da269baf4b130cf139de16c3
reyesjl <reyesjl.nine@hotmail.com>
2023-07-15 16:32:10 +0000

    product and catalog relaqted models loaded;

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+admin.site.register(Product) # let django know about the products model
+admin.site.register(Category) # let django know about the category model
