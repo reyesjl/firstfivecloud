8d38e9ffa2421d8d8f4df869e3ad0894f16142b6
reyesjl <reyesjl.nine@hotmail.com>
2023-07-17 04:06:57 +0000

    more format stuff

--- camps/views.py
+++ camps/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+
 def handleWelcome(request):
-  """
+    """
     Displays camps landing page
-  """
-  context = {"activelink": "camps"}
-  return render(request, "events/welcome.html", context)
+    """
+    context = {"activelink": "camps"}
+    return render(request, "events/welcome.html", context)
--- tours/views.py
+++ tours/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+
 def handleToursLanding(request):
-  """
+    """
     Displays tours landing page
-  """
-  context = {"activelink": "tours"}
-  return render(request, "tours_welcome.html", context)
+    """
+    context = {"activelink": "tours"}
+    return render(request, "tours_welcome.html", context)
