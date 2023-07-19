5b10fcd4f66e18b847b26e656315ede76196b435
reyesjl <reyesjl.nine@hotmail.com>
2023-07-19 15:48:23 +0000

    added my signup, login, and logout views

--- members/views.py
+++ members/views.py
@@ -1,30 +1,57 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-def handleLoginUser(request):
-    """
-    This view handles the login form.
-    """
+def handleSignup(request):
+    '''
+    Handles the signup of a new user
+    '''
     if request.method == "POST":
-        # Get the post parameters
-        loginusername = request.POST["loginusername"]
-        loginpassword = request.POST["loginpassword"]
+        # Get the POST data
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
 
-        user = authenticate(username=loginusername, password=loginpassword)
+        # Create the user
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+
+        # Log the user in
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "Successfully Logged In")
-            return redirect("home", {"user": user})
+            messages.success(request, "Successfully signed up!")
+            return redirect('home')
+    return render(request, 'authenticate/signup.html')
+        
+def handleLogin(request):
+    '''
+    Handles the login of an existing user
+    '''
+    if request.method == "POST":
+        # Get the POST data
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        
+        # If user is authenticated, log them in and redirect to home
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged in!")
+            return redirect('home')
         else:
-            messages.error(request, "Invalid Credentials, Please try again")
-            return redirect("login")
-    return render(request, "authenticate/login.html", {})
+            messages.error(request, "Invalid credentials, please try again.")
+            return redirect('login')
+    return render(request, 'authenticate/login.html')
 
-
-def handleSignupUser(request):
-    """
-    This view handles the signup form.
-    """
-    return render(request, "authenticate/signup.html", {})
+def handleLogout(request):
+    '''
+    Handles the logout of a user
+    '''
+    logout(request)
+    messages.success(request, "Successfully logged out!")
+    return redirect('home')
