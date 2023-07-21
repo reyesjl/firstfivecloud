59497d49d5e47affa95a443b9ff1a708c1bd3a59
reyesjl <reyesjl.nine@hotmail.com>
2023-07-21 15:39:20 +0000

    formatted; columns on services size;

--- members/views.py
+++ members/views.py
@@ -5,17 +5,19 @@ from django.contrib import messages
 
 
 def handleSignup(request):
-    '''
+    """
     Handles the signup of a new user
-    '''
+    """
     if request.method == "POST":
         # Get the POST data
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
 
         # Create the user
-        user = User.objects.create_user(username=username, password=password, email=email)
+        user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
         user.save()
 
         # Log the user in
@@ -23,35 +25,37 @@ def handleSignup(request):
         if user is not None:
             login(request, user)
             messages.success(request, "Successfully signed up!")
-            return redirect('home')
-    return render(request, 'authenticate/signup.html')
-        
+            return redirect("home")
+    return render(request, "authenticate/signup.html")
+
+
 def handleLogin(request):
-    '''
+    """
     Handles the login of an existing user
-    '''
+    """
     if request.method == "POST":
         # Get the POST data
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         # Authenticate the user
         user = authenticate(request, username=username, password=password)
-        
+
         # If user is authenticated, log them in and redirect to home
         if user is not None:
             login(request, user)
             messages.success(request, "Successfully logged in!")
-            return redirect('home')
+            return redirect("home")
         else:
             messages.error(request, "Invalid credentials, please try again.")
-            return redirect('login')
-    return render(request, 'authenticate/login.html')
+            return redirect("login")
+    return render(request, "authenticate/login.html")
+
 
 def handleLogout(request):
-    '''
+    """
     Handles the logout of a user
-    '''
+    """
     logout(request)
     messages.success(request, "Successfully logged out!")
-    return redirect('home')
+    return redirect("home")
