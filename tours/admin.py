81df1105fc836d2712951e56da1472a90c5d12ef
reyesjl <reyesjl.nine@hotmail.com>
2023-10-06 00:03:23 +0000

    deleted tours from master;

--- tours/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, EventInqueries, ToursInqueries
-
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(EventInqueries)
-admin.site.register(ToursInqueries)
--- tours/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.handleToursRoute, name="tours"),
-    path("globetrotter", views.handleInsuranceRoute, name="globetrotter"),
-]
