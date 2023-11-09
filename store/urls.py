1ab8fbc2a1f48565077a71f6df6efd6de16ed383
reyesjl <reyesjl.nine@hotmail.com>
2023-11-09 02:28:05 +0000

    form for team caps done;

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
   path("", views.handleStoreRoute, name="store"),
   path("caps/orderform/", views.handleCapsInquiry, name="capsorder"),
+  path("caps/sucess/", views.handleCapsSuccess, name="success_caps"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.core.mail import send_mail
 from .forms import CapsInquiryForm
 
 # Create your views here.
@@ -14,12 +15,38 @@ def handleStoreRoute(request):
 
 def handleCapsInquiry(request):
     if request.method == 'POST':
-        form = CapsInquiryForm(request.POST)
-        if form.is_valid():
-            caps_inquiry = form.save()
-            # Add any additional logic or redirection here
-            return redirect('success_page')  # Redirect to a success page or another appropriate URL
+      form = CapsInquiryForm(request.POST)
+      if form.is_valid():
+        caps_inquiry = form.save()
+
+        # Send a confirmation email
+        subject = 'Caps inquiry confirmation'
+        message = f"""Hey there {caps_inquiry.team_name},
+
+      Your caps order was submitted successfully. A representative will be reaching out shortly.
+
+      Have a great day, and thank you for your interest in team caps.
+      
+      First Five Rugby
+      www.firstfiverugby.com
+      letusknow@firstfiverugby.com
+        """
+        from_email = 'firstfiverugby@gmail.com'  # Replace with your email
+        recipient_list = [caps_inquiry.email]
+        send_mail(
+          subject,
+          message,
+          from_email,
+          recipient_list,
+          fail_silently=False,
+        )
+        
+        # Add any additional logic or redirection here
+        return redirect('success_caps')  # Redirect to a success page or another appropriate URL
     else:
-        form = CapsInquiryForm()
+      form = CapsInquiryForm()
 
     return render(request, 'capsform.html', {'form': form})
+
+def handleCapsSuccess(request):
+  return render(request, 'success.html', {})
