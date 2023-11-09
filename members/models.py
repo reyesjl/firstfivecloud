ca51b95a8461d9263769e49f1e7d3d695522c7d2
reyesjl <reyesjl.nine@hotmail.com>
2023-11-09 01:18:54 +0000

    profile fields;

--- members/models.py
+++ members/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class FirstfiveUser(AbstractUser):
   # Add your custom fields here, for example:
   bio = models.TextField(blank=True)
+  profile_photo = models.CharField(default="https://i.imgur.com/vuKfIWS.png")
+  level = models.IntegerField(default="0")
+  xp = models.IntegerField(default="0")
