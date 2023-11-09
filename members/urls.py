7c83591f9142cf7b2497bd41f7461db09d0e3ba1
reyesjl <reyesjl.nine@hotmail.com>
2023-11-09 01:20:03 +0000

    db migrations;

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+  path("", views.handleMembersRoute, name="members"),
+  path("profile/<str:username>", views.handleProfileRoute, name="profile"),
+  path("profile/edit/<int:user_id>", views.handleEditProfileRoute, name="alterprofile"),
   path("signup/", views.handleSignupUser, name="signup"),
   path("login/", views.handleLoginUser, name="login"),
   path("logout/", views.handleLogoutUser, name="logout"),
