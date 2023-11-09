704ead02d83d968884dae5b69a4ca9d4f0410c52
reyesjl <reyesjl.nine@hotmail.com>
2023-11-09 01:19:19 +0000

    edit profile; and handle dashboard;

--- members/views.py
+++ members/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model, login, logout
+from django.core.mail import send_mail
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
-from .forms import FFRSignupForm, FFRLoginForm
+
+from teams.models import Team
+from .forms import FFRSignupForm, FFRLoginForm, AlterUserForm
 from django.contrib.auth.decorators import login_required
 from f5.models import Event, EventTicket
 from .decorators import coach_required
@@ -14,9 +17,32 @@ def handleSignupUser(request):
         form = FFRSignupForm(request.POST)
         if form.is_valid():
             user = form.save()  # This will create and save the user to the database
-            login(request, user)  # Log in the user after registration
-            messages.success(request, 'User has been registered and logged in.')
-            return redirect('dashboard')  # Redirect to the home page after successful registration
+            # login(request, user)  # Log in the user after registration
+            messages.success(request, 'User has been registered')
+            
+            # Send a confirmation email
+            subject = 'Account Created'
+            message = f"""Welcome {user.username},
+
+        Your account was created successfully, please visit www.firstfiverugby.com/members/login 
+
+        Have a great day, and thank you for being a member!
+        
+        First Five Rugby
+        www.firstfiverugby.com
+        letusknow@firstfiverugby.com
+            """
+            from_email = 'firstfiverugby@gmail.com'  # Replace with your email
+            recipient_list = [user.email]
+            send_mail(
+                subject,
+                message,
+                from_email,
+                recipient_list,
+                fail_silently=False,
+            )
+
+            return redirect('login')  # Redirect to the home page after successful registration
     else:
         form = FFRSignupForm()
 
@@ -24,63 +50,108 @@ def handleSignupUser(request):
     
     return render(request, 'signup.html', context)
 
+def handleMembersRoute(request):
+    users = User.objects.all() 
+    context = {
+        'users': users,
+        'member_count': users.count(),
+    }
+    return render(request, "members.html", context)
 
-@login_required
-def handleDashboardRoute(request):
-    is_coach = request.user.groups.filter(name='coach').exists()
-    is_admin = request.user.groups.filter(name='admin').exists()
-    is_player = request.user.groups.filter(name='player').exists()
-
-    events = Event.objects.all()  # Get all events
-
-    selected_event = None
-    event_tickets = []
-
-    if request.method == 'POST':
-        event_id = request.POST.get('event')
-        if event_id:
-            selected_event = get_object_or_404(Event, pk=event_id)
-            event_tickets = EventTicket.objects.filter(camp=selected_event)
+def handleProfileRoute(request, username):
+    user = get_object_or_404(User, username=username)
 
     context = {
-        "user": request.user,
-        "events": events,
-        "event_tickets": event_tickets,
-        "selected_event": selected_event,
+        "profile": user,
     }
-    
-    if (is_coach):
-        return render(request, 'coach_dashboard.html', context)
-    elif (is_player):
-        return render(request, 'player_dashboard.html', context)
-    elif(is_admin):
-        return render(request, 'admin_dashboard.html', context)
+
+    return render(request, 'profile.html', context)
+
+@login_required
+def handleEditProfileRoute(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    if request.user != user:
+        # Redirect the user or display an error message
+        messages.error(request, "You are not allowed to edit this user's profile.")
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        form = AlterUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')  # Redirect to the profile page or another appropriate URL
+
     else:
-        return render(request, 'dashboard.html', context)
-    
-
-@login_required
-def handleCoachDashboardRoute(request):
-    events = Event.objects.all()  # Get all events
-
-    selected_event = None
-    event_tickets = []
-
-    if request.method == 'POST':
-        event_id = request.POST.get('event')
-        if event_id:
-            selected_event = get_object_or_404(Event, pk=event_id)
-            event_tickets = EventTicket.objects.filter(camp=selected_event)
+        form = AlterUserForm(instance=user)
 
     context = {
-        "user": request.user,
-        "events": events,
-        "event_tickets": event_tickets,
-        "selected_event": selected_event,
+        'profile': user,
+        'form': form,
     }
 
-    return render(request, 'dashboard.html', context)
+    return render(request, 'edit_profile.html', context)
 
+@login_required
+def handleDashboardRoute(request):    
+    user = request.user
+    context = {
+        "user": user,
+    }
+    
+    is_coach = user.groups.filter(name='coach').exists()
+    is_admin = user.groups.filter(name='admin').exists()
+    is_player = user.groups.filter(name='player').exists()
+
+    if is_coach:
+        coached_teams = Team.objects.filter(coach=user)
+        coached_events = Event.objects.filter(coach=user)
+        
+        events = Event.objects.all().order_by('-date')
+        selected_event = None
+        event_tickets = []
+        if request.method == 'POST':
+            event_id = request.POST.get('event')
+            if event_id:
+                selected_event = get_object_or_404(Event, pk=event_id)
+                event_tickets = EventTicket.objects.filter(camp=selected_event)
+        
+        context.update({
+            "coached_teams": coached_teams,
+            "coached_events": coached_events,
+            "selected_event": selected_event,
+            "event_tickets": event_tickets,
+        })
+        
+        return render(request, 'coach_dashboard.html', context)
+    
+    if is_player:
+        # Handle player-specific logic here
+        # Fetch player-related data and render 'player_dashboard.html'
+        return render(request, 'player_dashboard.html', context)
+    
+    if is_admin:
+        events = Event.objects.all().order_by('-date')
+        selected_event = None
+        event_tickets = []
+        if request.method == 'POST':
+            event_id = request.POST.get('event')
+            if event_id:
+                selected_event = get_object_or_404(Event, pk=event_id)
+                event_tickets = EventTicket.objects.filter(camp=selected_event)
+        
+        context.update({
+            "events": events,
+            "event_tickets": event_tickets,
+            "selected_event": selected_event,
+        })
+        # Handle admin-specific logic here
+        # Fetch admin-related data and render 'admin_dashboard.html'
+        return render(request, 'admin_dashboard.html', context)
+
+    # Default dashboard for users with no specific role
+    return render(request, 'dashboard.html', context)
+    
 def handleLoginUser(request):
     if request.method == "POST":
         form = FFRLoginForm(request, request.POST)
